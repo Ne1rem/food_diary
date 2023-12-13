5da26d37b61b66b081947d052d1f3309a81ca379
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-12-13 12:31:27 +0200

    в

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/HealthyHub">
+    <BrowserRouter basename="/food_diary">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
