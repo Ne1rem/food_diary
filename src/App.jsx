fdb9a08eefc0c110e48d42f5b76c115f9213d3ba
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-12-13 02:15:22 +0200

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { lazy } from "react";
-import './App.styled';
 import PublicRoute from "./route/PublicRoute/PublicRoute";
 import PrivateRoute from "./route/PrivateRoutes/PrivateRoutes";
 
