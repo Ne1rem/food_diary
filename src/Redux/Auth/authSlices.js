898c91af5f0f4cfcdb32f1ed33bdc701dff878e6
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-12-14 15:35:29 +0200

    header+ rudex

---
+++ src/Redux/Auth/authSlices.js
@@ -0,0 +1,58 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+
+// const initialState = {
+//   user: {
+//     name: null,
+//     email: null,
+//     goal: null,
+//     gender: null,
+//     age: null,
+//     height: null,
+//     weight: null,
+//     activity:null,
+//     avatarUrl:null,
+//     BMR:null
+//   },
+//   token: null,
+//   isLoggedIn: false,
+//   isRefreshing: false,
+// };
+  
+//   const authSlice = createSlice({
+//     name: 'auth',
+//     initialState,
+//     extraReducers: builder => {
+//       builder
+//         //signUP
+//         .addCase(signUp.pending, (state, { payload }) => {})
+//         .addCase(signUp.fulfilled, (state, { payload }) => {})
+//         .addCase(signUp.rejected, (state, { payload }) => {})
+
+//         //signIn
+//         .addCase(signIn.pending, (state, { payload }) => {})
+//         .addCase(signIn.fulfilled, (state, { payload }) => {})
+//         .addCase(signIn.rejected, (state, { payload }) => {})
+
+//         //LogOut
+//         .addCase(logOut.pending, (state, { payload }) => {})
+//         .addCase(logOut.fulfilled, (state, { payload }) => {})
+//         .addCase(logOut.rejected, (state, { payload }) => {})
+
+//         //UpdateUser
+//         .addCase(updateUser.pending, (state, { payload }) => {})
+//         .addCase(updateUser.fulfilled, (state, { payload }) => {})
+//         .addCase(updateUser.rejected, (state, { payload }) => {})
+
+//         //CheckEmail
+//         .addCase(checkEmail.pending, (state, { payload }) => {})
+//         .addCase(checkEmail.fulfilled, (state, { payload }) => {})
+//         .addCase(checkEmail.rejected, (state, { payload }) => {})
+
+//         //Refresh
+//         .addCase(refresh.fulfilled, (state, { payload }) => {})
+//         .addCase(refresh.rejected, (state, { payload }) => {})
+//     },
+//   });
+  
+//   export const authReducer = authSlice.reducer;
---
+++ src/Redux/store.js
@@ -0,0 +1,34 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+
+// import { authReducer } from './Auth/authSlices';
+
+// const tokenPersistConfig = {
+//   key: 'auth',
+//   storage,
+//   whitelist: ['token'],
+// };
+
+// export const store = configureStore({
+//   reducer: {
+//     auth: persistReducer(tokenPersistConfig, authReducer),
+//   },
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+
+// export const persistor = persistStore(store);
