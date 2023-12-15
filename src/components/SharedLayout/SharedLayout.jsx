94a87c925c66d4f9fd58a0f4d6848122b939bf3b
ivanbilobrov07 <ivanbilobrov07@gmail.com>
2023-12-16 01:09:43 +0200

    add main container for adaptive layout

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,13 +1,16 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from 'components/Header/Header';
+import { Container } from './SharedLayout.styled';
 
 const SharedLayout = () => {
   return (
     <>
       <Header />
       <Suspense fallback={null}>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Suspense>
     </>
   );
