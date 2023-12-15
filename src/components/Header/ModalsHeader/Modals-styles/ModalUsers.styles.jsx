f0868e4cb5d0ab15fc9e57fa94bea9092b00e84d
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2023-12-16 00:20:09 +0200

    і

--- src/components/Header/ModalsHeader/Modals-styles/ModalUsers.styles.jsx
+++ src/components/Header/ModalsHeader/Modals-styles/ModalUsers.styles.jsx
@@ -1,1 +1,48 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+export const ModalSettigns = styled.div`
+  position: fixed;
+  top: 80px;
+  right: 34px;
+  width: 158px;
+  height: 104px;
+  border: 1px;
+  border-radius: 12px;
+  box-shadow: 0px 4px 14px 0px #e3ffa833;
+  background-color: var(--color-primary-black-2);
+  z-index: 3;
+`;
+
+export const ModalPartButtonSettigns = styled.div`
+font-family: Poppins;
+display: flex;
+flex-direction: column;
+padding: 24px;
+gap: 16px;
+`
+
+export const ModalButtonSettigns = styled(Link)`
+display: flex;
+flex-direction: row;
+background-color: transparent;
+border-style: none;
+width: 110px;
+height: 20px;
+`;
+
+export const ModalSvgSettigns = styled.svg`
+width: 16px;
+height: 16px;
+fill: var(--color-primary-white);
+`
+
+export const ModalTextSettigns = styled.p`
+color: var(--color-primary-white);
+padding-left: 8px;
+font-size: 14px;
+font-weight: 500;
+line-height: 20px;
+letter-spacing: 0em;
+text-align: left;
+`
