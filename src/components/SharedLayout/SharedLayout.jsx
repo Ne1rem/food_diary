3bf669ee3f7eecfea3085d7716a267e9d30bfed2
ivanbilobrov07 <ivanbilobrov07@gmail.com>
2023-12-16 16:34:32 +0200

    create container component

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,16 +1,13 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from 'components/Header/Header';
-import { Container } from './SharedLayout.styled';
 
 const SharedLayout = () => {
   return (
     <>
       <Header />
       <Suspense fallback={null}>
-        <Container>
-          <Outlet />
-        </Container>
+        <Outlet />
       </Suspense>
     </>
   );
