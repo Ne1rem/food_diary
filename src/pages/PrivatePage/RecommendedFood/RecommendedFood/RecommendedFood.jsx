555278e442e3a229d682a438f666948c6c301d1e
OlegMalyshok <olegmalyshok@gmail.com>
2023-12-20 17:35:49 +0200

    RecommendedFood

--- src/pages/PrivatePage/RecommendedFood/RecommendedFood/RecommendedFood.jsx
+++ src/pages/PrivatePage/RecommendedFood/RecommendedFood/RecommendedFood.jsx
@@ -1,8 +1,5 @@
-import Avocado from '../../../../assets/recommendedfood/Avocado.png';
-import Beans from '../../../../assets/recommendedfood/Beans.png';
-import Nuts from '../../../../assets/recommendedfood/Nuts.png';
-import Broccoli from '../../../../assets/recommendedfood/Broccoli.png';
-import PhotoRecFood from '../../../../assets/recommendedfood/PhotoRecFood.png';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import {
   Calories,
   CharacteristicsContainer,
@@ -14,80 +11,25 @@ import {
   Title,
   WeightOfCalorieContainer,
 } from './RecommendedFoodStyled';
+import PhotoRecFood from '../../../../assets/recommendedfood/PhotoRecFood.png';
 
 const RecommendedFood = () => {
-  const foodItems = [
-    {
-      id: 1,
-      icon: Avocado,
-      name: 'Avocado',
-      measure: '100 g',
-      calories: '200 calories',
-    },
-    {
-      id: 2,
-      icon: Beans,
-      name: 'Beans',
-      measure: '100 g',
-      calories: '123 calories',
-    },
-    {
-      id: 3,
-      icon: Nuts,
-      name: 'Nuts',
-      measure: '100 g',
-      calories: '500 calories',
-    },
-    {
-      id: 4,
-      icon: Broccoli,
-      name: 'Broccoli',
-      measure: '100 g',
-      calories: '28 calories',
-    },
-    {
-      id: 1,
-      icon: Avocado,
-      name: 'Avocado',
-      measure: '100 g',
-      calories: '200 calories',
-    },
-    {
-      id: 2,
-      icon: Beans,
-      name: 'Beans',
-      measure: '100 g',
-      calories: '123 calories',
-    },
-    {
-      id: 3,
-      icon: Nuts,
-      name: 'Nuts',
-      measure: '100 g',
-      calories: '500 calories',
-    },
-    {
-      id: 4,
-      icon: Broccoli,
-      name: 'Broccoli',
-      measure: '100 g',
-      calories: '28 calories',
-    },
-    {
-      id: 1,
-      icon: Avocado,
-      name: 'Avocado',
-      measure: '100 g',
-      calories: '200 calories',
-    },
-    {
-      id: 2,
-      icon: Beans,
-      name: 'Beans',
-      measure: '100 g',
-      calories: '123 calories',
-    },
-  ];
+  const [foodItems, setFoodItems] = useState([]);
+
+  useEffect(() => {
+    const fetchRecommendedFood = async () => {
+      try {
+        const response = await axios.get(
+          'https://food-diary-backend-kr1b.onrender.com/api/recommended-food'
+        );
+        setFoodItems(response.data.slice(0, 10));
+      } catch (error) {
+        console.error('Error fetching recommended food:', error);
+      }
+    };
+
+    fetchRecommendedFood();
+  }, []);
 
   return (
     <Container>
@@ -97,11 +39,11 @@ const RecommendedFood = () => {
         <FoodCardsWrapper>
           {foodItems.map((item) => (
             <FoodCard key={item.id}>
-              <img src={item.icon} alt={item.name} />
+              <img src={item.img} alt={item.name} />
               <CharacteristicsContainer>
                 <h3>{item.name}</h3>
                 <WeightOfCalorieContainer>
-                  <p>{item.measure}</p>
+                  <p>{item.amount}</p>
                   <Calories>{item.calories}</Calories>
                 </WeightOfCalorieContainer>
               </CharacteristicsContainer>
