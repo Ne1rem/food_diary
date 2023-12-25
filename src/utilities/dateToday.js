60eb091dd95cf2482158b3b960feeb6581193a31
Anna <kotlyaranya1771@gmail.com>
2023-12-25 17:10:39 +0200

    (!)Redux & dataCharts fix

--- src/utilities/dateToday.js
+++
@@ -1,9 +0,0 @@
-const currentDate = new Date();
-
-const year = currentDate.getFullYear();
-
-const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-
-const day = String(currentDate.getDate()).padStart(2, '0');
-
-export const date = `${year}-${month}-${day}`;
