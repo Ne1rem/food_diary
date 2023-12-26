8f0d71d61ced43f92eb804518ff558cf104a8964
Anna <kotlyaranya1771@gmail.com>
2023-12-26 21:40:31 +0200

    main & readme

--- src/pages/PrivatePage/Main/Main.jsx
+++ src/pages/PrivatePage/Main/Main.jsx
@@ -1,9 +1,7 @@
 import { Container } from '../../../components/Container';
 import { DailyStatistics } from '../../../components/Main/DailyStatistics';
 import { MainHeader, Title, ContainerBottom } from './Main.styled';
-
 import GoalButtonOnMain from '../../../components/GoalButtonOnMain/GoalButtonOnMain';
-// import FoodCharts from '../../../components/FoodCharts/FoodCharts';
 import RecommendedFoodOnMain from '../RecommendedFood/RecFoodOnMain/RecFoodOnMain';
 import PartMainDiary from '../../../components/Main/Diary/PartMainDiary';
 
@@ -16,7 +14,6 @@ const Main = () => {
           <GoalButtonOnMain />
         </MainHeader>
         <DailyStatistics />
-        {/* <FoodCharts /> */}
         <ContainerBottom>
         <PartMainDiary />
         <RecommendedFoodOnMain />
@@ -27,4 +24,3 @@ const Main = () => {
 };
 
 export default Main;
-
